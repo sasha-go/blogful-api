c1a1805fb0ac8a38ea37bc62877a26f558e2ce44
sasha-go <sasha.lfg@gmail.com>
2020-04-21 20:46:46 -0700

    refactored articles-router and articles-service

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -1,9 +1,9 @@
 const { expect } = require("chai");
 const knex = require("knex");
 const app = require("../src/app");
-const { makeArticlesArray } = require('./articles.fixtures');
+const { makeArticlesArray } = require("./articles.fixtures");
 
-describe.only("Articles Endpoints", function () {
+describe("Articles Endpoints", function () {
   let db;
 
   before("make knext instance", () => {
@@ -19,24 +19,21 @@ describe.only("Articles Endpoints", function () {
   afterEach("cleanup", () => db("blogful_articles").truncate());
 
   describe(`GET /articles`, () => {
-	
-		context(`Given there are no articles`, () => {
-			it(`responds with 200 and an empty list`, () => {
-				return supertest(app)
-					.get('/articles')
-					.expect(200, [])
-			})
+    context(`Given there are no articles`, () => {
+      it(`responds with 200 and an empty list`, () => {
+        return supertest(app).get("/articles").expect(200, []);
+      });
 
-			context(`Given no articles`, () => {
-				it(`responds with 404`, () => {
-					const articleId = 123456
-					return supertest(app)
-						.get(`/articles/${articleId}`)
-						.expect(404, { error: { message: `Article doesn't exist`}})
-				})
-			})
-		})
-		
+      context(`Given no articles`, () => {
+        it(`responds with 404`, () => {
+          const articleId = 123456;
+          return supertest(app)
+            .get(`/articles/${articleId}`)
+            .expect(404, { error: { message: `Article doesn't exist` } });
+        });
+      });
+    });
+
     context("Given there are articles in the database", () => {
       const testArticles = makeArticlesArray();
       beforeEach("insert articles", () => {
@@ -65,8 +62,35 @@ describe.only("Articles Endpoints", function () {
           .expect(200, expectedArticle);
       });
     });
-	});
-	
+  });
 
+  describe(`POST /articles`, () => {
+    it(`creates an article, responding with 201 and the new articles`, function () {
+      this.retries(3);
+      const newArticle = {
+        title: "Test new article",
+        style: "Listicle",
+        content: "Test new article content...",
+      };
+      return supertest(app)
+        .post("/articles")
+        .send(newArticle)
+        .expect(201)
+        .expect((res) => {
+          expect(res.body.title).to.eql(newArticle.title);
+          expect(res.body.style).to.eql(newArticle.style);
+          expect(res.body.content).to.eql(newArticle.content);
+          expect(res.body).to.have.property("id");
+          expect(res.headers.location).to.eql(`/articles/${res.body.id}`);
+          const expected = new Date().toLocaleString();
+          const actual = new Date(res.body.date_published).toLocaleString();
+          expect(actual).to.eql(expected);
+        })
+        .then((postRes) =>
+          supertest(app)
+            .get(`/articles/${postRes.body.id}`)
+            .expect(postRes.body)
+        );
+    });
+  });
 });
-
